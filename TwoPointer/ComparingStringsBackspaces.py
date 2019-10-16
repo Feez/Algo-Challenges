06db9ae06b657ec0c29bdf4cf30f48ef0b522a97
Feez <dontmesswithfeez@gmail.com>
2019-10-15 20:46:43 -0400

    Fix backspace solution

--- TwoPointer/ComparingStringsBackspaces.py
+++ TwoPointer/ComparingStringsBackspaces.py
@@ -8,28 +8,29 @@ def backspace_compare(str1, str2):
     right1 = len(str1) - 1
     right2 = len(str2) - 1
 
-    while right1 >= 0 and right2 >= 0:
-
-        skip1 = 0
-        while right1 >= 0 and str1[right1] == "#":
-            right1 -= 1
-            skip1 += 1
-
-        skip2 = 0
-        while right2 >= 0 and str2[right2] == "#":
-            right2 -= 1
-            skip2 += 1
-
-        right1 -= skip1
-        right2 -= skip2
-
-        if right1 <= -1:
-            if right2 <= -1:
-                return True
-            return False
-        elif right2 <= -1:
-            return False
-
+    while right1 >= 0 or right2 >= 0:
+        
+        if str1[right1] == '#':
+            skip = 2
+            while skip > 0 and right1 >= 0:
+                right1 -= 1
+                if str1[right1] == '#':
+                    skip += 1
+                else:
+                    skip -= 1
+                    
+        if str2[right2] == '#':
+            skip = 2
+            while skip > 0 and right2 >= 0:
+                right2 -= 1
+                if str2[right2] == '#':
+                    skip += 1
+                else:
+                    skip -= 1
+                    
+        if right1 == -1 and right2 == -1:
+            return True
+            
         #print(f"Comparing '{str1[right1]}' and '{str2[right2]}'.")
         if str1[right1] != str2[right2]:
             return False
@@ -39,9 +40,9 @@ def backspace_compare(str1, str2):
 
     return True
 
-
-
-print(backspace_compare(str1="xy#z", str2="xzz#"))
-print(backspace_compare(str1="xy#z", str2="xyz#"))
-print(backspace_compare(str1="xp#", str2="xyz##"))
-print(backspace_compare(str1="xywrrmp", str2="xywrrmu#p"))
+print(backspace_compare(str1="xy#z", str2="xzz#"))          # True
+print(backspace_compare(str1="xy#z", str2="xyz#"))          # False
+print(backspace_compare(str1="xp#", str2="xyz##"))          # True
+print(backspace_compare(str1="xywrrmp", str2="xywrrmu#p"))  # True
+print(backspace_compare(str1="ab##", str2="c#d#"))          # True
+print(backspace_compare(str1="bxj##tw", str2="bxj###tw"))   # False
